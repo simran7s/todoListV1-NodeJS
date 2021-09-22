e00b82e37f8848a8f6c927c9f4d3564ecb6b6c15
Simran Sandhu <simran7s@my.yorku.ca>
2021-09-21 23:14:28 -0400

    setup adding of new item in todo list(li creation)

---
+++ app.js
@@ -0,0 +1,32 @@
+
+const express = require("express")
+const app = express()
+// Items that will be added to ToDo List
+let items = ["Buy food", "Make food", "Eat food"]
+// No need for requiring ejs
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", function (req, res) {
+    // res.sendFile(__dirname + "/index.html")
+    let today = new Date;
+    let options = {
+        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
+    }
+    let dayString = today.toLocaleDateString("en-US", options);
+    // res.render("name of ejs file located in views folder", "object of all variables in ejs file")
+    res.render("list", { dayOfWeek: dayString, newListItems: items });
+})
+
+
+app.post("/", function (req, res) {
+    console.log(req.body.newItem)
+    items.push(req.body.newItem);
+    res.redirect("/")
+})
+
+
+// Note: 3000 is an int, not a string.
+app.listen(process.env.PORT || 3000, () => {
+    console.log("App running on localhost:3000");
+})
